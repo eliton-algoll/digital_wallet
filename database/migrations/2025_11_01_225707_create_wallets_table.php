cdaadd38ef676911a3c74e5be3c840768edcc54a
Eliton Mendes <eliton.mendes@matamata.com.br>
2025-11-01 20:13:53 -0300

    feat: add default tables

---
+++ database/migrations/2025_11_01_225707_create_wallets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->float('balance')->default(0.0);
+            $table->float('daily_withdrawal_limit')->default(1000.0);
+            $table->float('daily_deposit_limit')->default(10000.0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
